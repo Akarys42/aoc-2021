0485be5d22b4705fab24487b7423df7a99af9efb
Matteo Bertucci <matteo.bertucci@etu.univ-amu.fr>
2021-12-02 09:24:27 +0000

    Day 1

---
+++ day_1/part_2.py
@@ -0,0 +1,15 @@
+with open('day_1/input.txt') as file:
+    INPUT = list(map(int, file.readlines()))
+
+previous = None
+increases = 0
+
+for i in range(3, len(INPUT) + 1):
+    current = sum(INPUT[i - 3:i])
+
+    if previous and previous < current:
+        increases += 1
+    
+    previous = current
+
+print(increases)
