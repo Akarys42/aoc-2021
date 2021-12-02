8520164d084203cd67788239808a0b202a6cacaf
root <root@fffed14f-10f6-411a-abfd-b816d6b21258.1yhbgidjtgjutpqmpjyjiwruic.ax.internal.cloudapp.net>
2021-12-02 09:22:59 +0000

    Pending changes exported from your codespace

---
+++ day_1/part_1.py
@@ -0,0 +1,13 @@
+with open('day_1/input.txt') as file:
+    INPUT = map(int, file.readlines())
+
+previous = None
+increases = 0
+
+for current in INPUT:
+    if previous and current > previous:
+        increases += 1
+
+    previous = current
+
+print(increases)
