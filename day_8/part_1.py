31e9683aa7ea7b7362cc3a7384e2657d091cf297
Matteo Bertucci <matteo.bertucci@etu.univ-amu.fr>
2021-12-09 15:58:13 +0000

    Day 8

---
+++ day_8/part_1.py
@@ -0,0 +1,17 @@
+from collections import Counter
+import re
+
+
+with open("day_8/input.txt") as file:
+    INPUT = re.sub(r".+ \|", "", file.read()).split()
+
+# This one is quite free, since all the digits we want
+# to count have a unique number of segments
+digit_lengths = Counter(len(digit) for digit in INPUT)
+
+# Mapping of unique length digits
+# 2 -> 1
+# 4 -> 4
+# 3 -> 7
+# 7 -> 8
+print(digit_lengths[2] + digit_lengths[4] + digit_lengths[3] + digit_lengths[7])
---
+++ day_8/part_2.py
@@ -0,0 +1,77 @@
+from functools import reduce
+import itertools
+from typing import Optional
+
+with open("day_8/input.txt") as file:
+    INPUT = [[part.split() for part in line.split(" | ")] for line in file.readlines()]
+
+# We note every segment as a bitmask to make things easier
+a = 1 << 0
+b = 1 << 1
+c = 1 << 2
+d = 1 << 3
+e = 1 << 4
+f = 1 << 5
+g = 1 << 6
+SEGMENTS = {"a": a, "b": b, "c": c, "d": d, "e": e, "f": f, "g": g}
+
+
+def pattern_to_hash(pattern: str) -> int:
+    hash_ = 0
+
+    for segment in pattern:
+        hash_ |= SEGMENTS[segment]
+
+    return hash_
+
+
+WIRES = [*"abcdefg"]
+DIGITS = [
+    "abcefg",
+    "cf",
+    "acdeg",
+    "acdfg",
+    "bcdf",
+    "abdfg",
+    "abdefg",
+    "acf",
+    "abcdefg",
+    "abcdfg",
+]
+HASH_TO_DIGIT = {pattern_to_hash(pattern): i for i, pattern in enumerate(DIGITS)}
+
+
+def validate_pattern(pattern: str, mapping: dict[str, str]) -> Optional[int]:
+    new_pattern = "".join(mapping[segment] for segment in pattern)
+
+    return HASH_TO_DIGIT.get(pattern_to_hash(new_pattern), None)
+
+
+total = 0
+
+for line in INPUT:
+    patterns = list(itertools.chain(*line))
+
+    resolved = False
+
+    for out_mapping in itertools.permutations(WIRES):
+        mapping = dict(zip(WIRES, out_mapping))
+
+        if not all(
+            validate_pattern(pattern, mapping) is not None for pattern in patterns
+        ):
+            continue
+
+        resolved = True
+
+        total += reduce(
+            lambda previous, pattern: previous * 10
+            + validate_pattern(pattern, mapping),
+            line[1],
+            0,
+        )
+        break
+
+    assert resolved
+
+print(total)
